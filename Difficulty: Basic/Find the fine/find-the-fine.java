e6a2c56bd9b23b0d5c7042a67d0b7e0b70a8658e
Sujan Gajanan Naik <211533922+SujanNaik890@users.noreply.github.com>
2026-08-27 00:54:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find the fine/find-the-fine.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public long totalFine(int date, int car[], int fine[]) {
+        // code here
+        int sum=0;
+        if(date %2==0){
+            for(int i=0;i<car.length;i++){
+                if(car[i]%2!=0)
+                    sum+=fine[i];
+            }
+        }else{
+            for(int i=0;i<car.length;i++){
+                if(car[i]%2==0)
+                    sum+=fine[i];
+            }
+        }
+        return sum;
+    }
+}
