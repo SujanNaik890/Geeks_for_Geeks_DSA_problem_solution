2c33d3fdaecc672818b1177b514628bb0371d048
Sujan Gajanan Naik <211533922+SujanNaik890@users.noreply.github.com>
2026-08-26 18:58:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.java
@@ -0,0 +1,10 @@
+class Solution {
+    int factorial(int n) {
+        // code here
+        int fact=1;
+        for(int i=n;i>=1;i--){
+            fact=fact*i;
+        }
+        return fact;
+    }
+}
