50047b5fe76fa0f47b9ec5817dd767e814685ba4
Sujan Gajanan Naik <211533922+SujanNaik890@users.noreply.github.com>
2026-09-06 22:13:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/First Occurrence in Sorted/first-occurrence-in-sorted.java
+++ Difficulty: Easy/First Occurrence in Sorted/first-occurrence-in-sorted.java
@@ -1,11 +1,8 @@
 class Solution {
     public int firstSearch(int[] arr, int k) {
         // Code Here
-           for(int i=0;i<arr.length;i++){
-            if(arr[i]==k){
-                return i;
-                
-            }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==k)return i;
         }
         return -1;
     }
