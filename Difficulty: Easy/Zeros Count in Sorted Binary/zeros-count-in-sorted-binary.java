a94c267281218f828df6d1e9adae3fe789458845
Sujan Gajanan Naik <211533922+SujanNaik890@users.noreply.github.com>
2026-09-07 21:48:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Zeros Count in Sorted Binary/zeros-count-in-sorted-binary.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countZeroes(int[] arr) {
+        // code here
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0)count++;
+        }
+        return count;
+        
+    }
+};
